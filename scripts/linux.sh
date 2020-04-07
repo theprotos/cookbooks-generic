68cacf10d5385603eaf10d3afbcb09b07792ef86
theprotos <vlcz@seznam.cz>
2020-04-08 00:33:33 +0200

    linux: added linux-cloud tools, update init script with timestamps

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -38,15 +38,15 @@ chef_client_rhel8="https://packages.chef.io/files/stable/chef/15.5.9/el/8/chef-1
 chef_client_rhel7="https://packages.chef.io/files/stable/chef/15.5.9/el/7/chef-15.5.9-1.el7.x86_64.rpm"
 chef_client_rhel6="https://packages.chef.io/files/stable/chef/15.5.9/el/6/chef-15.5.9-1.el6.x86_64.rpm"
 
-printf "    ==========[ OS Version ]==========\n"
+printf "$(date +%T) ==========[ OS Version ]==========\n"
 grep -w 'NAME' /etc/*-release
 grep -w 'VERSION' /etc/*-release
 
-printf "\n    ==========[ Update curl and git ]==========\n"
+printf "\n$(date +%T) ==========[ Update curl and git ]==========\n"
 #yum update -y -q -e 0
 yum install -y -q -e 0 curl git && printf "Done..\n"
 
-printf "\n    ==========[ Install chef-client ]==========\n"
+printf "\n$(date +%T) ==========[ Install chef-client ]==========\n"
 if ($( chef-client -v > /dev/null 2>&1 )); then
     printf "Already installed..\n"
 else
@@ -55,16 +55,16 @@ else
     chef-client --chef-license=accept > /dev/null 2>&1
 fi
 
-printf "\n    ==========[ Clone $branch from $repo to $tmp_dir ]==========\n"
+printf "\n$(date +%T) ==========[ Clone $branch from $repo to $tmp_dir ]==========\n"
 git clone $repo -b $branch --single-branch $tmp_dir/cookbooks-generic
 
 for rlist in $(echo $runlist | tr "," "\n")
 do
-  printf "\n    ==========[ Run chef-client with $rlist ]==========\n"
+  printf "\n$(date +%T) ==========[ Run chef-client with $rlist ]==========\n"
   chef-client -z -c $tmp_dir/cookbooks-generic/config.rb -j $tmp_dir/cookbooks-generic/$rlist
 done
 
-printf "\n    ==========[ Cleanup dir $tmp_dir ]==========\n"
+printf "\n$(date +%T) ==========[ Cleanup dir $tmp_dir ]==========\n"
 rm -rf $tmp_dir && printf "Done..\n"
 }
 
